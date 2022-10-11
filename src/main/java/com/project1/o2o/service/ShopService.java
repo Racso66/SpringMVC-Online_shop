b59e554a4ebcaeec51b4db2663130255114db061
Racso66 <43262362+Racso66@users.noreply.github.com>
2022-10-11 00:03:12 -0700

    Update ShopService.java
    Add two methods: modifyShop to update shop information, and getByShopId.

--- src/main/java/com/project1/o2o/service/ShopService.java
+++ src/main/java/com/project1/o2o/service/ShopService.java
@@ -7,5 +7,34 @@ import com.project1.o2o.entity.Shop;
 import com.project1.o2o.exceptions.ShopOperationException;
 
 public interface ShopService {
+	/**
+	 * get shop information through shopId
+	 * 
+	 * @param shopId
+	 * @return
+	 */
+	Shop getByShopId(long shopId);
+	
+	/**
+	 * Update shop information, including image processing
+	 * Any modifying of shop will return ShopExecution from dto
+	 * 
+	 * @param shop
+	 * @param shopImgInputStream
+	 * @param fileName
+	 * @return
+	 * @throws ShopOperationException
+	 */
+	ShopExecution modifyShop(Shop shop, InputStream shopImgInputStream, String fileName) throws ShopOperationException;
+	
+	/**
+	 * Register shop information, including image processing
+	 * 
+	 * @param shop
+	 * @param shopImgInputStream
+	 * @param fileName
+	 * @return
+	 * @throws ShopOperationException
+	 */
 	ShopExecution addShop(Shop shop, InputStream shopImgInputStream, String fileName) throws ShopOperationException;
 }

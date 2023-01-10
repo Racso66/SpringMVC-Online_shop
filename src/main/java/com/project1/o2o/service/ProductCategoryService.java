f54a8ed1bc1ebf3d88545891fc7e58f87469a738
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-10 14:52:02 -0800

    Add files via upload
    Added Product category service interface

---
+++ src/main/java/com/project1/o2o/service/ProductCategoryService.java
@@ -0,0 +1,14 @@
+package com.project1.o2o.service;
+
+import java.util.List;
+
+import com.project1.o2o.entity.ProductCategory;
+
+public interface ProductCategoryService {
+	/**
+	 * Search all product categories under the input shopId
+	 * @param shopId
+	 * @return List<ProductCategory>
+	 */
+	List<ProductCategory> getProductCategoryList(long shopId);
+}

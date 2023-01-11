a1572881eebd624adf2e139842338f234a6d20fd
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-11 14:42:57 -0800

    Update ProductCategoryService.java
    Added service delete product category to be implemented, reset product functionality to be implemented after product management is create.

--- src/main/java/com/project1/o2o/service/ProductCategoryService.java
+++ src/main/java/com/project1/o2o/service/ProductCategoryService.java
@@ -2,16 +2,36 @@ package com.project1.o2o.service;
 
 import java.util.List;
 
+import com.project1.o2o.dto.ProductCategoryExecution;
 import com.project1.o2o.entity.ProductCategory;
+import com.project1.o2o.exceptions.ProductCategoryOperationException;
 
 public interface ProductCategoryService {
 	/**
 	 * Search all product categories under the input shopId
+	 * 
 	 * @param shopId
 	 * @return List<ProductCategory>
+	 * @throws RuntimeException
 	 */
 	List<ProductCategory> getProductCategoryList(long shopId);
 	
+	/**
+	 * 
+	 * @param productCategoryList
+	 * @return
+	 * @throws ProductCategoryOperationException
+	 */
 	ProductCategoryExecution batchAddProductCategory(List<ProductCategory> productCategoryList)
-		throws ProductCategoryOperationException;
+			throws ProductCategoryOperationException;
+	
+	/**
+	 * This method needs to reset the product category id of the products under the category, then delete the category
+	 * @param productCategoryId
+	 * @param shopId
+	 * @return
+	 * @throws ProductCategoryOperationException
+	 */
+	ProductCategoryExecution deleteProductCategory(long productCategoryId, long shopId)
+			throws ProductCategoryOperationException;
 }

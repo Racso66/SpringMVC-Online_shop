6d742538d395ab35e4507fe4e85e4c9c7d9e20e2
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-10 23:24:28 -0800

    Update ProductCategoryDaoTest.java
    new JUnit test for batchInsertProductCategory method.

--- src/test/java/com/project1/o2o/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/project1/o2o/dao/ProductCategoryDaoTest.java
@@ -18,4 +18,24 @@ public class ProductCategoryDaoTest extends BaseTest{
 		List<ProductCategory> productCategoryList = productCategoryDao.queryProductCategoryList(shopId);
 		System.out.println("Number of customized categories: " + productCategoryList.size());
 	}
+	
+	@Test
+	public void testBatchInsertProductCategory() {
+		ProductCategory productCategory = new ProductCategory();
+		productCategory.setProductCategoryName("Product category 1");
+		productCategory.setPriority(1);
+		productCategory.setCreateTime(new Date());
+		productCategory.setShopId(2L);
+		ProductCategory productCategory2 = new ProductCategory();
+		productCategory2.setProductCategoryName("Product category 2");
+		productCategory2.setPriority(2);
+		productCategory2.setCreateTime(new Date());
+		productCategory2.setShopId(2L);
+		
+		List<ProductCategory> productCategoryList = new ArrayList<ProductCategory>();
+		productCategoryList.add(productCategory);
+		productCategoryList.add(productCategory2);
+		int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
+		assertEquals(2, effectedNum);
+	}
 }

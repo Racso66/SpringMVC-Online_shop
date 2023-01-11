0708322c4e1e7ad59e0f34b23c2a436a57de59f6
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-11 14:20:52 -0800

    Update ProductCategoryDaoTest.java
    Tested deletion of product categories and test order set to <<Insert->search->delete>>

--- src/test/java/com/project1/o2o/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/project1/o2o/dao/ProductCategoryDaoTest.java
@@ -1,26 +1,33 @@
 package com.project1.o2o.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.project1.o2o.BaseTest;
 import com.project1.o2o.entity.ProductCategory;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //Default, JVM, or Name ascending (ABCDEF...
 public class ProductCategoryDaoTest extends BaseTest{
 	@Autowired
 	private ProductCategoryDao productCategoryDao;
 	
 	@Test
-	public void testQueryByShopId() throws Exception {
+	public void testBQueryByShopId() throws Exception {
 		long shopId = 2;
 		List<ProductCategory> productCategoryList = productCategoryDao.queryProductCategoryList(shopId);
 		System.out.println("Number of customized categories: " + productCategoryList.size());
 	}
 	
 	@Test
-	public void testBatchInsertProductCategory() {
+	public void testABatchInsertProductCategory() {
 		ProductCategory productCategory = new ProductCategory();
 		productCategory.setProductCategoryName("Product category 1");
 		productCategory.setPriority(1);
@@ -37,5 +44,17 @@ public class ProductCategoryDaoTest extends BaseTest{
 		productCategoryList.add(productCategory2);
 		int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
 		assertEquals(2, effectedNum);
+	}
+	
+	@Test
+	public void testCDeleteProductCategory() throws Exception {
+		Long shopId = 2L;
+		List<ProductCategory> productCategoryList = productCategoryDao.queryProductCategoryList(shopId);
+		for(ProductCategory pc : productCategoryList) {
+			if("Product category 1".equals(pc.getProductCategoryName()) || "Product category 2".equals(pc.getProductCategoryName())) {
+				int effectedNum = productCategoryDao.deleteProductCategory(pc.getProductCategoryId(),shopId);
+				assertEquals(1, effectedNum);
+			}
+		}
 	}
 }

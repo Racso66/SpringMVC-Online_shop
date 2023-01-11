4966ba4c8c73728e2e6b7693fed6eebaddffaa4f
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-11 14:43:59 -0800

    Update ProductCategoryServiceImpl.java
    added implementation of delete product category service interface

--- src/main/java/com/project1/o2o/service/implementation/ProductCategoryServiceImpl.java
+++ src/main/java/com/project1/o2o/service/implementation/ProductCategoryServiceImpl.java
@@ -37,4 +37,18 @@ public class ProductCategoryServiceImpl implements ProductCategoryService{
 			}
 		} else return new ProductCategoryExecution(ProductCategoryStateEnum.EMPTY_LIST);
 	}
+	
+	@Override
+	@Transactional //wait until second step success before executing. Ensures both process submitted successfully
+	public ProductCategoryExecution deleteProductCategory(long productCategoryId, long shopId)
+			throws ProductCategoryOperationException{
+		//TODO reset the product category id of the products under the category
+		try {
+			int effectedNum = productCategoryDao.deleteProductCategory(productCategoryId, shopId);
+			if(effectedNum <= 0) throw new ProductCategoryOperationException("failed to delete product category");
+			else return new ProductCategoryExecution(ProductCategoryStateEnum.SUCCESS);
+		} catch(Exception e) {
+			throw new ProductCategoryOperationException("deleteProductCategory error: " + e.getMessage());
+		}
+	}
 }

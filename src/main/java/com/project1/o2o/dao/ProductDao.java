83747aa194715f57d99a5bc51c82d37ef089f2c9
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-11 23:49:30 -0800

    Add files via upload
    Insert product to be implemented

---
+++ src/main/java/com/project1/o2o/dao/ProductDao.java
@@ -0,0 +1,12 @@
+package com.project1.o2o.dao;
+
+import com.project1.o2o.entity.Product;
+
+public interface ProductDao {
+	/**
+	 * 
+	 * @param product
+	 * @return
+	 */
+	int insertProduct(Product product);
+}

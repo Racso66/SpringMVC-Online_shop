64cd02ef0489961ba71d1b29cd00b2f3734eb198
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-12 12:21:57 -0800

    Add files via upload
    Created runtime exception specifically for product operations

---
+++ src/main/java/com/project1/o2o/exceptions/ProductOperationException.java
@@ -0,0 +1,12 @@
+package com.project1.o2o.exceptions;
+
+public class ProductOperationException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7932516999381351495L;
+
+	public ProductOperationException(String msg) {
+		super(msg);
+	}
+}

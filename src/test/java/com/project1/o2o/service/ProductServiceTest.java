a75aac8ca9cd33f5606388b52a100920d470bdf8
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-17 22:26:11 -0800

    Update ProductServiceTest.java
    Added Junit test for modifyProduct method

--- src/test/java/com/project1/o2o/service/ProductServiceTest.java
+++ src/test/java/com/project1/o2o/service/ProductServiceTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -27,6 +28,7 @@ public class ProductServiceTest extends BaseTest {
 	private ProductService productService;
 	
 	@Test
+	@Ignore
 	public void testAAddProduct() throws ShopOperationException, FileNotFoundException{
 		Product product = new Product();
 		Shop shop = new Shop();
@@ -54,6 +56,35 @@ public class ProductServiceTest extends BaseTest {
 		productImgList.add(new ImageConstructor(productImg2.getName(), is2));
 		//Verify by adding the products
 		ProductExecution pe = productService.addProduct(product, thumbnail, productImgList);
+		assertEquals(ProductStateEnum.SUCCESS.getState(), pe.getState());
+	}
+	
+	@Test
+	public void testModifyProduct() throws ShopOperationException, FileNotFoundException{
+		Product product = new Product();
+		Shop shop = new Shop();
+		shop.setShopId(2L);
+		ProductCategory pc = new ProductCategory();
+		pc.setProductCategoryId(3L);
+		product.setProductId(2L);
+		product.setShop(shop);
+		product.setProductCategory(pc);
+		product.setProductName("Formal Product");
+		product.setProductDesc("Formal product");
+		//create thumbnail file input stream
+		File thumbnailFile = new File("C:\\Users\\SAO\\Desktop\\Dinoland pics\\4.png");
+		InputStream is = new FileInputStream(thumbnailFile);
+		ImageConstructor thumbnail = new ImageConstructor(thumbnailFile.getName(), is);
+		//test by adding 2 product specific image input stream and add to a list
+		File productImg1 = new File("C:\\Users\\SAO\\Desktop\\Dinoland pics\\3.png");
+		InputStream is1 = new FileInputStream(productImg1);
+		File productImg2 = new File("C:\\Users\\SAO\\Desktop\\Dinoland pics\\2.png");
+		InputStream is2 = new FileInputStream(productImg2);
+		List<ImageConstructor> productImgList = new ArrayList<ImageConstructor>();
+		productImgList.add(new ImageConstructor(productImg1.getName(), is1));
+		productImgList.add(new ImageConstructor(productImg2.getName(), is2));
+		//Verify by modifying the products
+		ProductExecution pe = productService.modifyProduct(product, thumbnail, productImgList);
 		assertEquals(ProductStateEnum.SUCCESS.getState(), pe.getState());
 	}
 }

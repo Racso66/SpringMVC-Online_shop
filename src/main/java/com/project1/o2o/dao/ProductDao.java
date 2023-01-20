b160f8cffd30c94a0ff71f459a96f08911434bb0
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-19 17:29:23 -0800

    Update ProductDao.java
    Added methods queryByProductId and queryProductList for searching, updateProduct for updating product info

--- src/main/java/com/project1/o2o/dao/ProductDao.java
+++ src/main/java/com/project1/o2o/dao/ProductDao.java
@@ -1,5 +1,9 @@
 package com.project1.o2o.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.project1.o2o.entity.Product;
 
 public interface ProductDao {
@@ -9,4 +13,37 @@ public interface ProductDao {
 	 * @return
 	 */
 	int insertProduct(Product product);
+
+	/**
+	 * get product information by productId
+	 * 
+	 * @param productId
+	 * @return
+	 */
+	Product queryByProductId(long productId);
+
+	/**
+	 * update product info
+	 * 
+	 * @param product
+	 * @return
+	 */
+	int updateProduct(Product product);
+	
+	/**
+	 * Search for list of products and separate to pages using following : Product name(fuzzy), status, shopId, category
+	 * @param productCondition
+	 * @param rowIndex
+	 * @param pageSize
+	 * @return
+	 */
+	List<Product> queryProductList(@Param("productCondition") Product productCondition, @Param("rowIndex") int rowIndex,
+			@Param("pageSize") int pageSize);
+	
+	/**
+	 * search for number of total products (with same condition as queryProductList, how many products exists in the list)
+	 * @param productCondition
+	 * @return
+	 */
+	int queryProductCount(@Param("productCondition") Product productCondition);
 }

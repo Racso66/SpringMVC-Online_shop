67440ef759b9be4fd741a99ebbce8951d3d7e2cd
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-23 21:05:49 -0800

    Add files via upload
    added getHeadLineList method to be implemented

---
+++ src/main/java/com/project1/o2o/service/HeadLineService.java
@@ -0,0 +1,17 @@
+package com.project1.o2o.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.project1.o2o.entity.HeadLine;
+
+public interface HeadLineService {
+	/**
+	 * Return head line list according to search conditions
+	 * 
+	 * @param headLineCondition
+	 * @return
+	 * @throws IOException
+	 */
+	List<HeadLine> getHeadLineList(HeadLine headLineCondition) throws IOException;
+}

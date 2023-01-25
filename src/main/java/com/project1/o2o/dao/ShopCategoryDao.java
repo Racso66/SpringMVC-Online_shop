fd875cb747eff3881653c93f7463031fd29c37d7
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-24 17:25:48 -0800

    Update ShopCategoryDao.java
    Added 5 methods

--- src/main/java/com/project1/o2o/dao/ShopCategoryDao.java
+++ src/main/java/com/project1/o2o/dao/ShopCategoryDao.java
@@ -7,5 +7,50 @@ import org.apache.ibatis.annotations.Param;
 import com.project1.o2o.entity.ShopCategory;
 
 public interface ShopCategoryDao {
+	/**
+	 * return shop category list based on search condition
+	 * 
+	 * @param shopCategoryCondition
+	 * @return
+	 */
 	List<ShopCategory> queryShopCategory(@Param("shopCategoryCondition") ShopCategory shopCategoryCondition);
+
+	/**
+	 * return shop category info through category id
+	 * 
+	 * @param shopCategoryId
+	 * @return
+	 */
+	ShopCategory queryShopCategoryById(long shopCategoryId);
+
+	/**
+	 * insert shop category information
+	 * 
+	 * @param shopCategory
+	 * @return
+	 */
+	int insertShopCategory(ShopCategory shopCategory);
+	
+	/**
+	 * update info
+	 * 
+	 * @param shopCategory
+	 * @return
+	 */
+	int updateShopCategory(ShopCategory shopCategory);
+
+	/**
+	 * 
+	 * @param shopCategoryId
+	 * @return
+	 */
+	int deleteShopCategory(long shopCategoryId);
+	
+	/**
+	 * batch delete category with list of category id
+	 * 
+	 * @param shopCategoryIdList
+	 * @return
+	 */
+	int batchDeleteShopCategory(List<Long> shopCategoryIdList);
 }

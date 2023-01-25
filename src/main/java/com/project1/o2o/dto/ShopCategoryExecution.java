24db63640e59e54c61ee9f0646dc5f33fc043ea9
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-24 17:43:50 -0800

    Add files via upload
    added execution class for shop category

---
+++ src/main/java/com/project1/o2o/dto/ShopCategoryExecution.java
@@ -0,0 +1,61 @@
+package com.project1.o2o.dto;
+
+import java.util.List;
+
+import com.project1.o2o.entity.ShopCategory;
+import com.project1.o2o.enums.ShopCategoryStateEnum;
+
+public class ShopCategoryExecution {
+	private int state;
+	private String stateInfo;
+	private ShopCategory shopCategory;
+	private List<ShopCategory> shopCategoryList;
+	
+	public ShopCategoryExecution() {
+		
+	}
+	
+	public ShopCategoryExecution(ShopCategoryStateEnum stateEnum) {
+		this.state = stateEnum.getState();
+		this.stateInfo = stateEnum.getStateInfo();
+	}
+	
+	public ShopCategoryExecution(ShopCategoryStateEnum stateEnum, List<ShopCategory> shopCategoryList) {
+		this.state = stateEnum.getState();
+		this.stateInfo = stateEnum.getStateInfo();
+		this.shopCategoryList = shopCategoryList;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public String getStateInfo() {
+		return stateInfo;
+	}
+
+	public void setStateInfo(String stateInfo) {
+		this.stateInfo = stateInfo;
+	}
+
+	public ShopCategory getShopCategory() {
+		return shopCategory;
+	}
+
+	public void setShopCategory(ShopCategory shopCategory) {
+		this.shopCategory = shopCategory;
+	}
+
+	public List<ShopCategory> getShopCategoryList() {
+		return shopCategoryList;
+	}
+
+	public void setShopCategoryList(List<ShopCategory> shopCategoryList) {
+		this.shopCategoryList = shopCategoryList;
+	}
+	
+}

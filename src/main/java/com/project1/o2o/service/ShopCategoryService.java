a17e18e6eb398065f621d75aed908045ccc2081f
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-24 18:56:54 -0800

    Update ShopCategoryService.java
    Added 3 new methods to be implemented

--- src/main/java/com/project1/o2o/service/ShopCategoryService.java
+++ src/main/java/com/project1/o2o/service/ShopCategoryService.java
@@ -2,9 +2,40 @@ package com.project1.o2o.service;
 
 import java.util.List;
 
+import com.project1.o2o.dto.ImageConstructor;
+import com.project1.o2o.dto.ShopCategoryExecution;
 import com.project1.o2o.entity.ShopCategory;
 
 public interface ShopCategoryService {
-		List<ShopCategory> getShopCategoryList(ShopCategory shopCategoryCondition);
+	/**
+	 * 
+	 * @param shopCategoryCondition
+	 * @return
+	 */
+	List<ShopCategory> getShopCategoryList(ShopCategory shopCategoryCondition);
+	
+	/**
+	 * add shopcategory and store category image
+	 * 
+	 * @param shopCategory
+	 * @param thumbnail
+	 * @return
+	 */
+	ShopCategoryExecution addShopCategory(ShopCategory shopCategory, ImageConstructor thumbnail);
 
+	/**
+	 * 
+	 * @param shopCategory
+	 * @param thumbnail
+	 * @return
+	 */
+	ShopCategoryExecution modifyShopCategory(ShopCategory shopCategory, ImageConstructor thumbnail);
+
+	/**
+	 * return category info based on id
+	 * 
+	 * @param shopCategoryId
+	 * @return
+	 */
+	ShopCategory getShopCategoryById(Long shopCategoryId);
 }

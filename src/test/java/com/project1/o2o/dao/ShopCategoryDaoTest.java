d3e1bf757ce9cbdd44e6dc8f0243d809e8c71a21
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-24 17:28:49 -0800

    Update ShopCategoryDaoTest.java
    Added Junit test for insertcategory and deletecategory

--- src/test/java/com/project1/o2o/dao/ShopCategoryDaoTest.java
+++ src/test/java/com/project1/o2o/dao/ShopCategoryDaoTest.java
@@ -1,5 +1,8 @@
 package com.project1.o2o.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,10 +14,33 @@ import com.project1.o2o.entity.ShopCategory;
 public class ShopCategoryDaoTest extends BaseTest{
 	@Autowired
 	private ShopCategoryDao shopCategoryDao;
+	
 	@Test
-	public void testQueryShopCategory() {
+	public void testBQueryShopCategory() {
 		List<ShopCategory> shopCategoryList = shopCategoryDao.queryShopCategory(null);
 		System.out.println(shopCategoryList.size());
 	}
-
+	
+	@Test
+	public void testAInsertShopCategory() {
+		ShopCategory shopCategory = new ShopCategory();
+		shopCategory.setShopCategoryName("Sushi");
+		shopCategory.setShopCategoryDesc("Japanese sushi");
+		shopCategory.setShopCategoryImg("Test image");
+		shopCategory.setPriority(1);
+		shopCategory.setCreateTime(new Date());
+		
+		int effectedNum = shopCategoryDao.insertShopCategory(shopCategory);
+		assertEquals(1, effectedNum);
+	}
+	
+	@Test
+	public void testCDeleteShopCategory() throws Exception {
+		Long shopCategoryId = 4L;
+		ShopCategory shopCategory = shopCategoryDao.queryShopCategoryById(shopCategoryId);
+		if("Sushi".equals(shopCategory.getShopCategoryName())){
+			int effectedNum = shopCategoryDao.deleteShopCategory(shopCategoryId);
+			assertEquals(1, effectedNum);
+		}
+	}
 }

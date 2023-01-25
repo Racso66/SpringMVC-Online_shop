6e5964d68115de92726a565845c6733c33e0d6af
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-01-24 17:51:13 -0800

    Add files via upload
    added shopcategoryoperationexception extends runtime exception

---
+++ src/main/java/com/project1/o2o/exceptions/ShopCategoryOperationException.java
@@ -0,0 +1,13 @@
+package com.project1.o2o.exceptions;
+
+public class ShopCategoryOperationException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1451417839308601278L;
+
+	public ShopCategoryOperationException(String msg) {
+		super(msg);
+	}
+}

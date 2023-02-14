4a802be4546cb6b35cbe7a49d9ec1bfe176ce203
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-13 19:04:57 -0800

    Add files via upload
    Added dto for wechatauth

---
+++ src/main/java/com/project1/o2o/dto/WechatAuthExecution.java
@@ -0,0 +1,77 @@
+package com.project1.o2o.dto;
+
+import java.util.List;
+
+import com.project1.o2o.entity.WechatAuth;
+import com.project1.o2o.enums.WechatAuthStateEnum;
+
+public class WechatAuthExecution {
+	private int state;
+	private String stateInfo;
+	private int count;
+	private WechatAuth wechatAuth;
+	private List<WechatAuth> wechatAuthList;
+	
+	public WechatAuthExecution() {
+		
+	}
+	
+	//Constructor for failure
+	public WechatAuthExecution(WechatAuthStateEnum stateEnum) {
+		this.state = stateEnum.getState();
+		this.stateInfo = stateEnum.getStateInfo();
+	}
+	
+	public WechatAuthExecution(WechatAuthStateEnum stateEnum, WechatAuth wechatAuth) {
+		this.state = stateEnum.getState();
+		this.stateInfo = stateEnum.getStateInfo();
+		this.wechatAuth = wechatAuth;
+	}
+	
+	public WechatAuthExecution(WechatAuthStateEnum stateEnum, List<WechatAuth> wechatAuthList) {
+		this.state = stateEnum.getState();
+		this.stateInfo = stateEnum.getStateInfo();
+		this.wechatAuthList = wechatAuthList;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public String getStateInfo() {
+		return stateInfo;
+	}
+
+	public void setStateInfo(String stateInfo) {
+		this.stateInfo = stateInfo;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public WechatAuth getWechatAuth() {
+		return wechatAuth;
+	}
+
+	public void setWechatAuth(WechatAuth wechatAuth) {
+		this.wechatAuth = wechatAuth;
+	}
+
+	public List<WechatAuth> getWechatAuthList() {
+		return wechatAuthList;
+	}
+
+	public void setWechatAuthList(List<WechatAuth> wechatAuthList) {
+		this.wechatAuthList = wechatAuthList;
+	}
+	
+}

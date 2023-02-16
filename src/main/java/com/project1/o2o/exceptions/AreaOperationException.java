b0d6043f11ef2eac7863024681f2671dba3e8c65
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-16 01:32:52 -0800

    Add files via upload
    Added AreaOperationException as child runtime exception

---
+++ src/main/java/com/project1/o2o/exceptions/AreaOperationException.java
@@ -0,0 +1,13 @@
+package com.project1.o2o.exceptions;
+
+public class AreaOperationException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5946450524112738397L;
+	
+	public AreaOperationException(String msg) {
+		super(msg);
+	}
+}

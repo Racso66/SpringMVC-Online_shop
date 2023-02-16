a0a02d09f9283f4d19ef4d6219a9161fa2e389cb
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-16 01:33:37 -0800

    Update AreaServiceImpl.java
    Added code to distribute database load to Redis

--- src/main/java/com/project1/o2o/service/implementation/AreaServiceImpl.java
+++ src/main/java/com/project1/o2o/service/implementation/AreaServiceImpl.java
@@ -1,24 +1,70 @@
 package com.project1.o2o.service.implementation;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project1.o2o.cache.JedisUtil;
 import com.project1.o2o.dao.AreaDao;
 import com.project1.o2o.entity.Area;
+import com.project1.o2o.exceptions.AreaOperationException;
 import com.project1.o2o.service.AreaService;
 
 @Service
 public class AreaServiceImpl implements AreaService {
 	@Autowired
 	private AreaDao areaDao;
-
+	@Autowired
+	private JedisUtil.Keys jedisKeys;
+	@Autowired
+	private JedisUtil.Strings jedisStrings;
+	
+	private static String AREALISTKEY = "arealist";
+	private static Logger logger = LoggerFactory.getLogger(AreaServiceImpl.class);
+	
 	@Override
+	@Transactional
 	public List<Area> getAreaList() {
-		// return areaDao.queryArea when getAreaList() is returned to retrieve area from
-		// idbc
-		return areaDao.queryArea();
+		String key = AREALISTKEY;
+		List<Area> areaList = null;
+		// store areaList target to redis. Convert List to Strings then convert Strings back to return type
+		ObjectMapper mapper = new ObjectMapper();
+		if(!jedisKeys.exists(key)) {
+			areaList = areaDao.queryArea();
+			String jsonString;
+			try {
+				jsonString = mapper.writeValueAsString(areaList);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+				logger.error(e.getMessage());
+				throw new AreaOperationException(e.getMessage());
+			}
+			jedisStrings.set(key, jsonString);
+		} else { //key already exists
+			String jsonString = jedisStrings.get(key);
+			JavaType javaType = mapper.getTypeFactory().constructParametricType(ArrayList.class, Area.class);
+			try {
+				areaList = mapper.readValue(jsonString, javaType);
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+				logger.error(e.getMessage());
+				throw new AreaOperationException(e.getMessage());
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+				logger.error(e.getMessage());
+				throw new AreaOperationException(e.getMessage());
+			}
+		}
+		return areaList;
 	} // implement interface, extends class
 
 }

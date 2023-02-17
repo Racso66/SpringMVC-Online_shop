55c41e1edbbb22f4621d838a592359aeba8dacda
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-16 16:56:38 -0800

    Update AreaService.java
    Moved key pair from implementation to interface

--- src/main/java/com/project1/o2o/service/AreaService.java
+++ src/main/java/com/project1/o2o/service/AreaService.java
@@ -5,5 +5,7 @@ import java.util.List;
 import com.project1.o2o.entity.Area;
 
 public interface AreaService {
+	public static final String AREALISTKEY = "arealist";
+	
 	List<Area> getAreaList();
 }

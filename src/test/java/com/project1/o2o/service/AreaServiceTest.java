bb1c2002636ad949adbb9a88877e4ee2528c98b5
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-16 18:23:03 -0800

    Update AreaServiceTest.java
    Added Junit test for cacheService method removeFromCache.

--- src/test/java/com/project1/o2o/service/AreaServiceTest.java
+++ src/test/java/com/project1/o2o/service/AreaServiceTest.java
@@ -8,14 +8,20 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.project1.o2o.BaseTest;
+import com.project1.o2o.cache.JedisUtil;
 import com.project1.o2o.entity.Area;
 
 public class AreaServiceTest extends BaseTest{
 	@Autowired
 	private AreaService areaService;
+	@Autowired
+	private CacheService cacheService;
+	
 	@Test
 	public void testGetAreaList() {
 		List<Area>areaList = areaService.getAreaList();
 		assertEquals("West_Vancouver",areaList.get(0).getAreaName());
+		cacheService.removeFromCache(areaService.AREALISTKEY);
+		areaList = areaService.getAreaList();
 	}
 }

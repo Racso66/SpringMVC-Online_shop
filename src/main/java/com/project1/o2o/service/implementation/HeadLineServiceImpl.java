8f08e29fa765245860a83782ea9fbf0aa9de4445
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-16 16:59:59 -0800

    Update HeadLineServiceImpl.java
    moved key pair to interface. Added redis keys and strings class to cache search info

--- src/main/java/com/project1/o2o/service/implementation/HeadLineServiceImpl.java
+++ src/main/java/com/project1/o2o/service/implementation/HeadLineServiceImpl.java
@@ -1,22 +1,76 @@
 package com.project1.o2o.service.implementation;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project1.o2o.cache.JedisUtil;
 import com.project1.o2o.dao.HeadLineDao;
 import com.project1.o2o.entity.HeadLine;
+import com.project1.o2o.exceptions.HeadLineOperationException;
 import com.project1.o2o.service.HeadLineService;
 
 @Service
 public class HeadLineServiceImpl implements HeadLineService {
 	@Autowired
 	private HeadLineDao headLineDao;
+	@Autowired
+	private JedisUtil.Keys jedisKeys;
+	@Autowired
+	private JedisUtil.Strings jedisStrings;
+	
+	private static Logger logger = LoggerFactory.getLogger(HeadLineServiceImpl.class);
 	
 	@Override
+	@Transactional
 	public List<HeadLine> getHeadLineList(HeadLine headLineCondition) throws IOException {
-		return headLineDao.queryHeadLine(headLineCondition);
+		String key = HLLISTKEY;
+		List<HeadLine> headLineList = null;
+		ObjectMapper mapper = new ObjectMapper();
+		//Because headline has condition, need to append status to key
+		if(headLineCondition != null && headLineCondition.getEnableStatus() != null)
+			key = key + "_" + headLineCondition.getEnableStatus();
+		if(!jedisKeys.exists(key)) {
+			headLineList = headLineDao.queryHeadLine(headLineCondition);
+			// record all values classes in string format and store into redis
+			String jsonString;
+			try {
+				jsonString = mapper.writeValueAsString(headLineList);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+				logger.error(e.getMessage());
+				throw new HeadLineOperationException(e.getMessage());
+			}
+			jedisStrings.set(key, jsonString);
+		} else {
+			// Extract string value stored in according key in redis
+			String jsonString = jedisStrings.get(key);
+			// appoint types to be converted to from String, Jackson package
+			JavaType javaType = mapper.getTypeFactory().constructParametricType(ArrayList.class, HeadLine.class);
+			try {
+				headLineList = mapper.readValue(jsonString, javaType);
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+				logger.error(e.getMessage());
+				throw new HeadLineOperationException(e.getMessage());
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+				logger.error(e.getMessage());
+				throw new HeadLineOperationException(e.getMessage());
+			}
+		}
+		return headLineList;
 	}
+	
+	
 }

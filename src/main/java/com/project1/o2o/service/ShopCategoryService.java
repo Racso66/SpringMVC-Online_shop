79e58f4c2a24177f5b1666fa3a94e98f6a86a47c
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-16 16:57:43 -0800

    Update ShopCategoryService.java
    moved key pair from impl to interface

--- src/main/java/com/project1/o2o/service/ShopCategoryService.java
+++ src/main/java/com/project1/o2o/service/ShopCategoryService.java
@@ -7,6 +7,8 @@ import com.project1.o2o.dto.ShopCategoryExecution;
 import com.project1.o2o.entity.ShopCategory;
 
 public interface ShopCategoryService {
+	public static final String SCLISTKEY = "shopcategorylist";
+	
 	/**
 	 * 
 	 * @param shopCategoryCondition

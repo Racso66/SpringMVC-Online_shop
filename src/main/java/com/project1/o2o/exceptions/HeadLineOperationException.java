3d04597c2f708a51ceb7fed7c60539e51b4d6588
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-16 16:55:54 -0800

    Add files via upload
    Added HeadLineOperationException as Runtime Exception for hl operations

---
+++ src/main/java/com/project1/o2o/exceptions/HeadLineOperationException.java
@@ -0,0 +1,13 @@
+package com.project1.o2o.exceptions;
+
+public class HeadLineOperationException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7228177128818300295L;
+	
+	public HeadLineOperationException(String msg) {
+		super(msg);
+	}
+}

a8db03cabc6833169c4f4fd85fb5105b507da342
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-21 02:56:13 -0800

    Add files via upload
    Added Operation exception for localauth as runtime exception

---
+++ src/main/java/com/project1/o2o/exceptions/LocalAuthOperationException.java
@@ -0,0 +1,13 @@
+package com.project1.o2o.exceptions;
+
+public class LocalAuthOperationException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -324025727244613612L;
+	
+	public LocalAuthOperationException(String msg) {
+		super(msg);
+	}
+}

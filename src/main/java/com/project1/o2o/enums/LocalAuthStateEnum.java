aeb5adb3c03fa0c8320a3c60a70ada9cabad67c2
Racso66 <43262362+Racso66@users.noreply.github.com>
2023-02-21 02:57:32 -0800

    Add files via upload
    Added State enum for localauth

---
+++ src/main/java/com/project1/o2o/enums/LocalAuthStateEnum.java
@@ -0,0 +1,30 @@
+package com.project1.o2o.enums;
+
+public enum LocalAuthStateEnum {
+	LOGINFAIL(-1, "wrong password or username"), SUCCESS(0, "success"),NULL_AUTH_INFO(-1006, "empty account information"),
+	ONLY_ONE_ACCOUNT(-1007, "each wechat can only bind one account");
+	
+	private int state;
+	private String stateInfo;
+	private LocalAuthStateEnum(int state, String stateInfo) {
+		this.state = state;
+		this.stateInfo = stateInfo;
+	}
+	public int getState() {
+		return state;
+	}
+
+	public String getStateInfo() {
+		return stateInfo;
+	}
+
+	public static LocalAuthStateEnum stateOf(int index) {
+		for(LocalAuthStateEnum state : values()) {
+			if(state.getState() == index) {
+				return state;
+			}
+		}
+		return null;
+	}
+	
+}
